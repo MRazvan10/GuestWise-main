61d36951630fa12ef86ee3221bf0ca10bb8b4baf
razvanmocanu <razvan.mocanu@thoughtworks.com>
2023-10-31 23:03:58 +0200

    initial interface

--- app/src/main/java/com/main/guestwise/MainActivity.kt
+++ app/src/main/java/com/main/guestwise/MainActivity.kt
@@ -3,16 +3,25 @@ package com.main.guestwise
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -24,6 +33,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.main.guestwise.ui.theme.GuestWiseTheme
@@ -43,31 +54,68 @@ class MainActivity : ComponentActivity() {
 fun MyApp(modifier: Modifier = Modifier) {
 
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    var expanded by remember { mutableStateOf(false) }
+    val languages = listOf(
+        Pair(R.drawable.en, "en"),
+        Pair(R.drawable.ro, "ro"),
+        Pair(R.drawable.fr, "fr"),
+        Pair(R.drawable.es, "es"),
+        Pair(R.drawable.pt, "pt"),
+        Pair(R.drawable.de, "de")
+    )
+    var selectedIndex by remember { mutableStateOf(0) }
 
     Surface(modifier) {
-        if (shouldShowOnboarding) {
-            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-        } else {
-            Greetings()
+        Column {
+            Row {
+                Box(Modifier.size(50.dp)) {
+                    Image(painter = painterResource(id = R.drawable.guestwise), contentDescription = null,
+                        modifier = Modifier.clickable { println("Button Clicked!") })
+                }
+                Spacer(Modifier.weight(1f))
+                Box(Modifier.padding(10.dp)) {
+                    Image(
+                        painter = painterResource(id = languages[selectedIndex].first), contentDescription = null,
+                        modifier = Modifier.clickable(onClick = { expanded = true })
+                    )
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false },
+                        modifier = Modifier
+                            .width(50.dp)
+                            .wrapContentHeight()
+                    ) {
+                        languages.forEachIndexed { index, s ->
+                            DropdownMenuItem(onClick = {
+                                selectedIndex = index
+                                expanded = false
+                            }, text = {
+                                Text(text = s.second)
+                            })
+                        }
+                    }
+                }
+            }
+            if (shouldShowOnboarding) {
+                OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+            } else {
+                Greetings()
+            }
         }
     }
 }
 
 @Composable
 fun OnboardingScreen(
-    onContinueClicked: () -> Unit,
-    modifier: Modifier = Modifier
+    onContinueClicked: () -> Unit, modifier: Modifier = Modifier
 ) {
 
     Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Welcome to GuestWise!")
+        Text(stringResource(R.string.welcome_to_guestwise))
         Button(
-            modifier = Modifier.padding(top = 12.dp),
-            onClick = onContinueClicked
+            modifier = Modifier.padding(top = 12.dp), onClick = onContinueClicked
         ) {
             Text("Register")
         }
@@ -81,9 +129,7 @@ fun OnboardingScreen(
 
 @Composable
 private fun Greetings(
-    modifier: Modifier = Modifier,
-    name: String = "Name",
-    items: List<String> = List(20) { "$it" }
+    modifier: Modifier = Modifier, name: String = "Name", items: List<String> = List(20) { "$it" }
 ) {
     Column {
         Greeting(name = name)
@@ -115,15 +161,13 @@ private fun Greeting(name: String) {
     val extraPadding = if (expanded.value) 48.dp else 0.dp
 
     Surface(
-        color = MaterialTheme.colorScheme.background,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        color = MaterialTheme.colorScheme.background, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding),
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .padding(bottom = extraPadding), horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
@@ -136,13 +180,11 @@ private fun Greeting(name: String) {
 private fun Item(name: String) {
 
     Surface(
-        color = MaterialTheme.colorScheme.background,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        color = MaterialTheme.colorScheme.background, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Column(modifier = Modifier.padding(24.dp)) {
             Button(
-                onClick = { },
-                modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+                onClick = { }, modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
             ) {
                 Text(name)
             }
